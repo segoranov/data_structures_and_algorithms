5b214673739569d6ff8918029863fb6cc1566c7d
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-04-30 15:24:19 +0300

    Implement constructor specifying the size of the dynamic array with unit test

--- data_structures/dynamic_array/dynamic_array.cpp
+++ data_structures/dynamic_array/dynamic_array.cpp
@@ -1,5 +1,13 @@
 #include "dynamic_array.h"
 
+DynamicArray::DynamicArray(size_t initialSize) {
+  m_arr = new double[initialSize];
+  m_currentSize = initialSize;
+  m_currentCapacity = initialSize;
+}
+
+size_t DynamicArray::size() const { return m_currentSize; }
+
 size_t DynamicArray::capacity() const { return m_currentCapacity; }
 
 bool DynamicArray::empty() const { return m_currentSize == 0; }
--- data_structures/dynamic_array/unit_tests/dynamic_array_utest.cpp
+++ data_structures/dynamic_array/unit_tests/dynamic_array_utest.cpp
@@ -8,3 +8,10 @@ TEST_CASE("Dynamic array is empty and has no buffer on construction with "
   REQUIRE(arr.capacity() == 0);
   REQUIRE(!arr.data());
 }
+
+TEST_CASE("Dynamic array has correct size when using size constructor") {
+  DynamicArray arr{23};
+  REQUIRE(!arr.empty());
+  REQUIRE(arr.capacity() >= 23);
+  REQUIRE(arr.size() == 23);
+}
