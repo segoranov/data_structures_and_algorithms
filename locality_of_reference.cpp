dd352ce039105bb791f5fdda25e4a0ace40c4519
Stiliyan Goranov <33035110+stiliangoranov@users.noreply.github.com>
2020-04-30 21:04:07 +0300

    Improve program description
    Clarify what the program shows about 'locality of reference'

--- locality_of_reference.cpp
+++ locality_of_reference.cpp
@@ -2,15 +2,32 @@
 #include <iostream>
 
 /*
-Program to demonstrate the concept of locality.
+Program to demonstrate the concept 'locality of reference'
 https://en.wikipedia.org/wiki/Locality_of_reference
 
-Calculating the sum of all elements of 40'000 x 40'000 matrix in two ways:
+The program calculates the sum of all elements of large (in this case 40'000 x 40'000) matrix in two ways:
 1. Row by row
 2. Column by column
 
 The second method is WAY slower than the first because of locality of reference
 and how the matrix (represented by array) is layed out in memory.
+
+The key idea is that when we access an array's element, the CPU fetches from RAM
+to CACHE memory (which is a lot faster) not only that element, but others surrounding it.
+
+When we traverse the array sequentially (corresponding to 'row by row'), the access to
+most of the elements is way faster, because they are prefetched from RAM in CACHE. 
+That is to say that access to most of the elements elicits 'cache hit'.
+Thus the hardware works in our benefit.
+
+When we traverse the array not sequentially (corresponding to 'column by column'), the access
+to most of the elements is slower, i.e. causing 'cache miss', and the CPU has to fetch them from
+RAM, because our program does not take advantage of the prefetched surrounding elements.
+Thus the hardware works against us.
+
+The below measurement is not 'correct' per se, because it uses time as a reference, and this
+time can depend on millions of other things (like the OS, the number of processes running, and so on...),
+but even so, we can observe the big difference in the two ways of traversal.
 
 -------------
 
@@ -28,7 +45,7 @@ Time taken without locality: 58 seconds.
 -------------
 
 As you can tell, when the size of the matrix gets larger (more than 100 000 x
-100 000), this difference becomes dramatic.
+100 000), this difference becomes even more dramatic.
 */
 int main() {
   const unsigned numRows = 40'000;
