420f343a2b20d6bc685278c1663cad0238ae3efe
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-05-01 22:31:03 +0300

    Update doubly linked list demo

--- data_structures/doubly_linked_list/doubly_linked_list_demo.cpp
+++ data_structures/doubly_linked_list/doubly_linked_list_demo.cpp
@@ -1,7 +1,36 @@
 #include "doubly_linked_list.h"
 #include <iostream>
 
+// the reference is not const because const_iterator is
+// not implemented
+template <typename T> void print(DoublyLinkedList<T> &list) {
+  DoublyLinkedListIterator<T> it = list.begin();
+  for (; it != list.end(); ++it) {
+    std::cout << *it << ' ';
+  }
+
+  // it points to the last element now
+  std::cout << *it << std::endl;
+}
+
 int main() {
   DoublyLinkedList<int> list;
+  for (int i = 1; i <= 10; i++) {
+    list.push_back(i);
+  }
+
+  print(list);
+
+  int sum = 0;
+  DoublyLinkedListIterator<int> it = list.begin();
+  for (; it != list.end(); ++it) {
+    sum += *it;
+  }
+
+  // it points to the last element now
+  sum += *it;
+
+  std::cout << "Sum of elements: " << sum << std::endl;
+
   return 0;
 }
