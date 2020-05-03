f81836bd666ad8cc95a188e621a4068d4e0c480a
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-05-03 19:20:09 +0300

    Add unit test for BST

--- data_structures/binary_search_tree/unit_tests/binary_search_tree_utest.cpp
+++ data_structures/binary_search_tree/unit_tests/binary_search_tree_utest.cpp
@@ -6,3 +6,18 @@ TEST_CASE(
   BinarySearchTree<int> bst;
   REQUIRE(bst.isEmpty());
 }
+
+TEST_CASE("Add two elements to BST and then make the tree empty") {
+  BinarySearchTree<int> bst;
+  bst.insert(5);
+  bst.insert(6);
+
+  REQUIRE(!bst.isEmpty());
+  REQUIRE(bst.contains(5));
+  REQUIRE(bst.contains(6));
+  REQUIRE(bst.findMax() == 6);
+  REQUIRE(bst.findMin() == 5);
+
+  bst.makeEmpty();
+  REQUIRE(bst.isEmpty());
+}
