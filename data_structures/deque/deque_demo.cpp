2cc00e96f4d40de6a6203215338eaccb4086a4e1
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-05-09 23:23:16 +0300

    Update deque demo

--- data_structures/deque/deque_demo.cpp
+++ data_structures/deque/deque_demo.cpp
@@ -1,44 +1,8 @@
 #include "deque.h"
 #include <iostream>
 
-template <typename T> void print(const Deque<T> &deque) {
-  if (deque.empty()) {
-    std::cout << "The deque is empty.\n";
-    return;
-  }
-
-  std::cout << "Size: " << deque.size() << "; elements: ";
-
-  for (int i = 0; i < deque.size(); i++) {
-    std::cout << deque[i] << ' ';
-  }
-
-  std::cout << '\n';
-}
-
 int main() {
-  // Deque<double> deque;
-
-  // for (int i = 1; i <= 10; i++) {
-  //   deque.push_back(static_cast<double>(i));
-  // }
-
-  // double sum = 0;
-  // for (int i = 0; i < deque.size(); i++) {
-  //   sum += deque[i];
-  // }
-
-  // print(deque);
-  // std::cout << "Sum of deque elements is: " << sum << '\n';
-
-  // for (int i = 1; i <= 10; i++) {
-  //   print(deque);
-  //   deque.pop_back();
-  // }
-
-  // print(deque);
-  // deque.clear();
-  // print(deque);
+  /// EXAMPLES CAN BE SEEN IN THE UNIT TESTS
 
   return 0;
 }
