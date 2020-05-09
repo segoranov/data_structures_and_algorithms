0aa24d16bd278f684d2284808f32ffbe5358f6d6
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-05-09 11:40:46 +0300

    [WIP] Start work on deque. CURRENTLY NOT COMPILING!
    - Define the interface
    - Create basic unit tests

---
+++ data_structures/deque/deque.h
@@ -0,0 +1,125 @@
+#ifndef DEQUE_H_20200509
+#define DEQUE_H_20200509
+
+template <typename T> class Deque {
+public:
+  Deque() = default;
+  Deque(size_t initialSize);
+  Deque(const Deque &);
+  Deque &operator=(const Deque &);
+  ~Deque();
+
+  /**
+   * Appends the given element value to the end of the container.
+   */
+  void push_back(const T &value);
+
+  /**
+   * Removes the last element of the container.
+   * Calling pop_back on an empty container is undefined behavior.
+   */
+  void pop_back();
+
+  /**
+   * Appends the given element value to the beginning of the container.
+   */
+  void push_front(const T &value);
+
+  /**
+   * Removes the first element of the container.
+   * Calling pop_back on an empty container causes undefined behavior.
+   */
+  void pop_front();
+
+  /**
+   * Checks if index is out of bounds and throws exception if it is
+   * @throw std::out_of_range
+   */
+  T &at(size_t index);
+
+  /**
+   * Access element without checking if index is out of bounds
+   * Accessing out of bounds element causes undefined behavior.
+   */
+  T &operator[](size_t index);
+  const T &operator[](size_t index) const;
+
+  /**
+   * Returns a reference to the last element in the container.
+   * Calling back on an empty container is undefined behavior.
+   */
+  T &back();
+  const T &back() const;
+
+  /**
+   * Returns a reference to the first element in the container.
+   * Calling front on an empty container is undefined behavior.
+   */
+  T &front();
+  const T &front() const;
+
+  /**
+   * Returns number of elements in the deque
+   */
+  size_t size() const;
+
+  /**
+   * Returns the size of the internal buffer.
+   * Could be larger than the number of elemets in the deque
+   */
+  // size_t capacity() const; TODO is it needed? std::deque lacks this.
+
+  /**
+   * Returns whether the deque is empty of elements or not
+   */
+  bool empty() const;
+
+  /**
+   * Erases all elements from the container. After this call, size() returns
+   * zero.
+   */
+  void clear();
+
+private:
+  T *m_arr = nullptr;
+  size_t m_currentSize = 0;
+  size_t m_currentCapacity = 0;
+};
+
+template <typename T> Deque<T>::Deque(size_t initialSize) {}
+
+template <typename T> Deque<T>::Deque(const Deque &other) {}
+
+template <typename T> Deque<T> &Deque<T>::operator=(const Deque &other) {}
+
+template <typename T> void Deque<T>::push_back(const T &value) {}
+
+template <typename T> void Deque<T>::pop_back() {}
+
+template <typename T> size_t Deque<T>::size() const {}
+
+// template <typename T> size_t Deque<T>::capacity() const {}
+
+template <typename T> bool Deque<T>::empty() const {}
+
+template <typename T> void Deque<T>::clear() {}
+
+template <typename T> T &Deque<T>::at(size_t index) {}
+
+template <typename T> T &Deque<T>::operator[](size_t index) {}
+
+template <typename T> const T &Deque<T>::operator[](size_t index) const {}
+
+template <typename T> T &Deque<T>::back() {}
+
+template <typename T> const T &Deque<T>::back() const {}
+
+template <typename T> Deque<T>::~Deque() { delete[] m_arr; }
+
+template <typename T>
+bool operator==(const Deque<T> &lhs, const Deque<T> &rhs) {}
+
+template <typename T>
+bool operator!=(const Deque<T> &lhs, const Deque<T> &rhs) {}
+
+#endif
---
+++ data_structures/deque/unit_tests/deque_utest.cpp
@@ -0,0 +1,151 @@
+#include "catch.hpp"
+#include "deque.h"
+
+TEST_CASE("Deque is empty and has no buffer on construction with "
+          "default constructor") {
+  Deque<double> deque;
+  REQUIRE(deque.empty());
+}
+
+TEST_CASE("Deque has correct size when using size constructor") {
+  Deque<double> deque{23};
+  REQUIRE(!deque.empty());
+  REQUIRE(deque.size() == 23);
+}
+
+TEST_CASE("Deque has correct size after adding many elements") {
+  Deque<double> deque;
+
+  for (int i = 0; i < 20'000; i++) {
+    deque.push_back(i);
+    deque.push_front(-i);
+  }
+
+  REQUIRE(!deque.empty());
+  REQUIRE(deque.size() == 40'000);
+}
+
+TEST_CASE("Push back and push front two elements in deque") {
+  Deque<double> deque;
+  deque.push_back(0);
+  deque.push_front(1);
+
+  REQUIRE(deque[0] == 1);
+  REQUIRE(deque[1] == 0);
+}
+
+TEST_CASE("Deque's front and back element access functions work correctly") {
+  Deque<double> deque;
+
+  double val_back = 1.0;
+  double val_front = -1.0;
+  for (int i = 0; i < 30; i++) {
+    deque.push_back(val_back);
+    deque.push_front(val_front);
+    REQUIRE(deque.back() == val_back);
+    REQUIRE(deque.front() == val_front);
+    ++val_back;
+    --val_front;
+  }
+
+  REQUIRE(deque.size() == 60);
+
+  double val = -30;
+  for (int i = 0; i < deque.size(); i++) {
+    if (val == 0) {
+      ++val;
+    }
+    REQUIRE(deque[i] == val);
+    REQUIRE(deque.at(i) == val);
+    ++val;
+  }
+}
+
+TEST_CASE("Deque's element access function at() throws exception when "
+          "index is out of bounds") {
+  Deque<double> deque;
+  for (int i = 0; i < 155; i++) {
+    deque.push_back(69.0);
+  }
+
+  for (int i = 0; i < 155; i++) {
+    REQUIRE_NOTHROW(deque.at(i));
+  }
+
+  REQUIRE_THROWS_AS(deque.at(155), std::out_of_range);
+}
+
+TEST_CASE("Deque's clear() works properly") {
+  Deque<double> deque;
+  for (int i = 0; i < 150; i++) {
+    deque.push_front(i);
+    deque.push_back(69.0);
+  }
+
+  REQUIRE(deque.size() == 300);
+
+  deque.clear();
+
+  // deque should be empty; size should be 0;
+  REQUIRE(deque.empty());
+  REQUIRE(deque.size() == 0);
+
+  // should not be able to access any element
+  for (int i = -300; i < 300; i++) {
+    REQUIRE_THROWS_AS(deque.at(i), std::out_of_range);
+  }
+}
+
+TEST_CASE("Comparators of deques work correctly") {
+  Deque<double> deque;
+  for (int i = 0; i < 155; i++) {
+    deque.push_front(169.0);
+    deque.push_back(69.0);
+  }
+
+  Deque<double> deque1;
+  for (int i = 0; i < 155; i++) {
+    deque.push_front(169.0);
+    deque1.push_back(69.0);
+  }
+
+  REQUIRE(deque1 == deque);
+
+  Deque<double> deque2;
+  for (int i = 0; i < 155; i++) {
+    deque.push_back(static_cast<double>(i));
+  }
+
+  REQUIRE(deque2 != deque);
+}
+
+TEST_CASE("Deque's copy constructor and operator equals work properly") {
+  Deque<double> deque;
+  for (int i = 0; i < 155; i++) {
+    deque.push_front(169.0);
+    deque.push_back(69.0);
+  }
+
+  Deque<double> deque_copy{deque};
+  REQUIRE(deque_copy == deque);
+
+  Deque<double> deque_equals;
+  deque_equals = deque;
+  REQUIRE(deque_equals == deque);
+}
+
+TEST_CASE("Pop back works correctly") {
+  Deque<double> deque;
+  for (int i = 0; i < 155; i++) {
+    deque.push_back(69.0);
+  }
+
+  REQUIRE(deque.size() == 155);
+
+  for (int i = 155; i > 0; i--) {
+    REQUIRE(deque.size() == i);
+    deque.pop_back();
+  }
+
+  REQUIRE(deque.empty());
+}
